555dc336288b2eb788d03b5ad1c121255041f6a2
xentoro00 <diorhyseni0@gmail.com>
2024-05-30 16:16:46 +0200

    Added a new route

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors());
 // Root route
 app.get('/', (req, res) => {
   res.send('Hello, world!');
